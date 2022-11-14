4650258633aed37c9db5940f215040f9e7b8bf53
MunozatABI <51766200+MunozatABI@users.noreply.github.com>
2022-11-14 16:33:43 +1300

    Update temporal_buffer.jl

--- temporal_buffer.jl
+++ temporal_buffer.jl
@@ -1,11 +1,11 @@
 using Unitful 
 
-struct TemporalBuffer{T}
+mutable struct TemporalBuffer{T}
 	dt   # Smallest time interval
 	buf::Array{T, 2}
 end 
 
-function advance!(tb::TemporalBuffer)
+function advance!(tb::TemporalBuffer{T}) where T
 	nr, nc = size(tb.buf)
 	tb.buf = hcat(zeros(T, nr), tb.buf[:, 1:nc-1])
 end 
@@ -18,3 +18,7 @@ end
 function set_index!(tb::TemporalBuffer, x, i)
 	tb.buf[i, 1] = x
 end
+
+function Base.copy(tb::TemporalBuffer)
+    return TemporalBuffer(tb.dt, copy(tb.buf))
+end 
