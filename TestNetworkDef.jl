8cb70c96dadaf39392643fa7bcfa9d780dca23a5
al.nejati <al.nejati@soulmachines.com>
2022-11-15 13:17:58 +1300

    Some cleanup

--- TestNetworkDef.jl
+++
@@ -1,5 +0,0 @@
-include("defineNetwork.jl")
-#Network = defineNetwork.connectivity
-#print(size(Network.weights[("4E", "5E")]))
-Network = defineNetwork.delays
-print(keys(Network.pops))
--- test_network_def.jl
+++ test_network_def.jl
@@ -1,5 +1,4 @@
 include("CorticalNetworkDefinition.jl")
 network = CorticalNetworkDefinition.connectivity
 print(size(network.weights[("4E", "5E")]))
-Network = CorticalNetworkDefinition.delays
 print(network.pops)
