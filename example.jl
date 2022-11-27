521fb707ee5c2f91ccd5d4274ba6a8a1ca689d80
al.nejati <al.nejati@soulmachines.com>
2022-11-28 07:41:47 +1300

    Use dicts so that key names match

--- example.jl
+++ example.jl
@@ -1,9 +1,11 @@
+
 using Pkg 
 Pkg.develop(path="./Simulator")
 Pkg.develop(path="./NetworkDefinition")
 
 using NetworkDefinition
 using Simulator 
+using SparseArrays
 using Statistics
 using Plots
 
@@ -16,67 +18,33 @@ inputs = Dict("2/3E" => (87.8, 8f0*2000), "2/3I" => (87.8, 8f0*1850),
               "5E" => (87.8, 8f0*2000), "5I" => (87.8, 8f0*1850),
               "6E" => (87.8, 8f0*2000), "6I" => (87.8, 8f0*1850))
 
+pop_names = keys(net.pops)
+
 sim_time = 0.25 #seconds
 timesteps = round(Int, sim_time/1e-4)
-spike_array_23E = zeros(Float32, length(net.pops["2/3E"].current), timesteps)
-spike_array_23I = zeros(Float32, length(net.pops["2/3I"].current), timesteps)
-spike_array_4E = zeros(Float32, length(net.pops["4E"].current), timesteps)
-spike_array_4I = zeros(Float32, length(net.pops["4I"].current), timesteps)
-voltage_array_5E = zeros(Float32, length(net.pops["5E"].current), timesteps)
-spike_array_5E = zeros(Float32, length(net.pops["5E"].current), timesteps)
-spike_array_5I = zeros(Float32, length(net.pops["5I"].current), timesteps)
-spike_array_6E = zeros(Float32, length(net.pops["6E"].current), timesteps)
-spike_array_6I = zeros(Float32, length(net.pops["6I"].current), timesteps)
+
+spike_array = Dict(key => spzeros(Float32, length(net.pops[key].current), timesteps) for key in pop_names)
+voltage_array = Dict(key => zeros(Float32, length(net.pops[key].current), timesteps) for key in pop_names)
 
 for i in 1:timesteps       
     sim_step!(net, inputs)
-    spike_array_23E[:, i] = net.pops["2/3E"].spike.buf[:, 1]
-    spike_array_23I[:, i] = net.pops["2/3I"].spike.buf[:, 1]
-    spike_array_4E[:, i] = net.pops["4E"].spike.buf[:, 1]
-    spike_array_4I[:, i] = net.pops["4I"].spike.buf[:, 1]
-    voltage_array_5E[:, i] = net.pops["5E"].voltage
-    spike_array_5E[:, i] = net.pops["5E"].spike.buf[:, 1]
-    spike_array_5I[:, i] = net.pops["5I"].spike.buf[:, 1]
-    spike_array_6E[:, i] = net.pops["6E"].spike.buf[:, 1]
-    spike_array_6I[:, i] = net.pops["6I"].spike.buf[:, 1]
+    
+    for key in pop_names
+        spike_array[key][:, i] = net.pops[key].spike[:]
+        voltage_array[key][:, i] = net.pops[key].voltage
+    end 
 end
 
 #@show spike_array
-sum_spike_23E = sum(spike_array_23E[:, 500:end], dims=1)
-println(sum(sum_spike_23E))
-sum_spike_23I = sum(spike_array_23I[:, 500:end], dims=1)
-println(sum(sum_spike_23I))
-sum_spike_4E = sum(spike_array_4E[:, 500:end], dims=1)
-println(sum(sum_spike_4E))
-sum_spike_4I = sum(spike_array_4I[:, 500:end], dims=1)
-println(sum(sum_spike_5I))
-sum_spike_5E = sum(spike_array_5E[:, 500:end], dims=1)
-println(sum(sum_spike_5E))
-sum_spike_5I = sum(spike_array_5I[:, 500:end], dims=1)
-println(sum(sum_spike_5I))
-sum_spike_6E = sum(spike_array_6E[:, 500:end], dims=1)
-println(sum(sum_spike_6E))
-sum_spike_6I = sum(spike_array_6I[:, 500:end], dims=1)
-println(sum(sum_spike_6I))
+sum_spike = Dict(key => sum(spike_array[key][:, 500:end], dims=1) for key in pop_names)
 
-mean_spike_23E = Statistics.mean(sum_spike_23E)
-println(mean_spike_23E)
-mean_spike_23I = Statistics.mean(sum_spike_23I)
-println(mean_spike_23I)
-mean_spike_4E = Statistics.mean(sum_spike_4E)
-println(mean_spike_4E)
-mean_spike_4I = Statistics.mean(sum_spike_4I)
-println(mean_spike_4I)
-mean_spike_5E = Statistics.mean(sum_spike_5E)
-println(mean_spike_5E)
-mean_spike_5I = Statistics.mean(sum_spike_5I)
-println(mean_spike_5I)
-mean_spike_6E = Statistics.mean(sum_spike_6E)
-println(mean_spike_6E)
-mean_spike_6I = Statistics.mean(sum_spike_6I)
-println(mean_spike_6I)
+sum_spike_epoch = Dict(key => sum(sum_spike[key]) for key in pop_names)
+mean_spike_epoch = Dict(key => Statistics.mean(sum_spike[key]) for key in pop_names)
 
-a = plot(sum_spike_23E[1, :])
+@show sum_spike_epoch
+@show mean_spike_epoch
+
+a = plot(sum_spike["2/3E"][1, :])
 #a = plot(current_array[20, :])
 #c = plot(spike_array[20, :])
 @show a
